af90aec3eb4ce267128adc7fe24b633e3de03baa
annaboyatyuk <annaboyatyuk@gmail.com>
2018-05-24 15:33:17 -0700

    the tests pass but theres uncovered lines so idk if its right

--- __test__/lib/reader.spec.js
+++ __test__/lib/reader.spec.js
@@ -1,123 +1,96 @@
 'use strict';
 
 const read = require('../../lib/reader.js');
-
+  
 describe('Reader Module', () => {
-
+  
   it('returns error when given a bad file name', (done) => {
-
-    let file = `${__dirname}/../../data/bad.txt`;
-
+  
+    let file = 'bad.txt';
+  
     read(file, (err) => {
-      expect(err).toBeDefined();
+      expect(err).toThrowError();
     });
     done();
   });
 
-  it('returns the contents of a file that exists', (done) => {
 
-    let file = `data/letters.txt`;
+  it('returns an error for any non existent file', (done) => {
 
-    read(file, (err, data) => {
-      expect(typeof data).toBe('string');
+
+    read('data/letters.txt', 'data/missing.txt', (err) => {
+
+      expect(err).not.toBeNull();
     });
     done();
   });
 
-  it('returns the contents of the files when given an array', (done) => {
 
-    let paths = [`data/letters.txt`, `data/words.txt`];
+
+  it('returns the contents of the files in a string when given an array', (done) => {
+
+    let expected = 'words words words words words';
+
+    read('data/words.txt', (err, words) => {
+      expect(err).toBeNull();
+
+      const actual = words[0].toString();
+
+      expect(actual).toBe(expected);
+
+    });
+    done();
+  });
+
+
+  it('returns the contents of multiple files in order', (done) => {
+
+    let paths = ['data/ipsum.txt', 'data/words.txt', 'data/letters.txt'];
+
     let path = '';
+    let textArr = [];
     paths.forEach((ele) => {
       path = ele.toString();
       read(path, (err, data) => {
-        expect(typeof data).toBe('string');
+        textArr.push(data);
+        console.log(textArr[1]);
+        expect(textArr[0]).toContain('ipsum');
+        expect(textArr[1]).toContain('words');
+        expect(textArr[2]).toContain('abcde');
       });
       done();
     });
+
+    // let paths = [];
+
+    // for( let item of ['ipsum', 'words', 'letters']) {
+    //   paths.push(`data/'${item}'.txt`);
+    // }
+
+    // let expected, actual;
+
+    // read(paths, (err, contents) => {
+
+    //   expect(err).toBeNull();
+
+    //   expected = true;
+    //   actual = contents[0].toString().toContain('ipsum');
+    //   expect(actual).toBe(expected);
+
+    //   expected = 'words words words words words';
+    //   actual = contents[1].toString();
+    //   expect(actual).toBe(expected);
+
+    //   expected = 'abcdefghijklmnopqrstuvwxyz';
+    //   actual = contents[2].toString();
+    //   expect(actual).toBe(expected);
+
+    // });
+    // done();
   });
+
 
 });
 
 
-
-// const read = require('../../lib/reader');
-
-// describe('Reader Module', () => {
-
-//   it('should callback with error for a non-existent file', (done) => {
-
-//     read(['missing.txt'], (err) => {
-
-//       expect(err).not.toBeNull();
-      
-//       done();
-      
-//     });
-//   });
-
-//   it('should callback with error for any non-existent file', (done) => {
-
-//     read([__dirname + '/data/apples.txt', 'missing.txt'], (err) => {
-
-//       expect(err).not.toBeNull();
-      
-//       done();
-//     });
-//   });
-
-//   it('should callback with file contents of one file', (done) => {
-  
-//     const expected = 'words about apples'; 
-
-//     read([__dirname + '/../data/apples.txt'], (err, contents) => {
-
-//       expect(err).toBeNull();
-
-//       const actual = contents[0].toString();
-
-//       expect(actual).toBe(expected);
-
-//       done();
-      
-//     });
-//   });
-
-//   xit('should callback with file contents of multiple files in order', (done) => {
-  
-//     let paths = [];
-
-//     // NOTE: cucumber.txt has a lot of text in it so should load slower
-//     for (let item of ['cucumbers','apples','bananas']) {
-//       paths.push(__dirname + '/../data/' + item + '.txt');
-//     }
-
-//     let expected, actual;
-
-//     read(paths, (err, contents) => {
-
-//       expect(err).toBeNull();
-
-//       expected = true;
-
-//       actual = contents[0].toString().startsWith('cucumber');
-
-//       expect(actual).toBe(expected);
-
-//       expected = 'words about apples'; 
-      
-//       actual = contents[1].toString();
-
-//       expect(actual).toBe(expected);
-
-//       expected = 'words about bananas'; 
-      
-//       actual = contents[2].toString();
-
-//       expect(actual).toBe(expected);
-
-//       done();      
-//     });
-//   });
-// });
 
